b67c588ce841cf675234808e7f8499344f826672
Situ William <situ_william@yahoo.com>
2017-11-28 15:20:44 -0500

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   resources :products do
-  	resources :reviews
+  	resources :reviews, except: [:index, :new, :show]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.create!(name: "1992 BMW M3", description: "Imola red BMW M3 from 1992.", price: 20000, url: "https://s1.cdn.autoevolution.com/images/gallery/BMWM3-E30--770_11.jpg")
 Product.create!(name: "Porsche 918 Spyder", description: "Targa top Porsche Supercar", price: 1000000, url: "https://buyersguide.caranddriver.com/media/assets/submodel/4320.jpg")
+
+Review.create!(comment: "wow this car is cool", product_id: 2)
+Review.create!(product_id: 2, comment: "wow this car is okay")
+Review.create!(product_id: 2, comment: "wow this car is cool")
+Review.create!(product_id: 3, comment: "wow this car is okay")
+Review.create!(product_id: 3, comment: "wow this car is cool")
+Review.create!(product_id: 3, comment: "wow this car is okay")
