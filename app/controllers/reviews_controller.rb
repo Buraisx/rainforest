3d77659e8ca468d6db397d1391f8dea2661a72da
Situ William <situ_william@yahoo.com>
2017-11-28 16:41:20 -0500

    added delete comment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,4 +6,27 @@ class ReviewsController < ApplicationController
 		@review.save
 		redirect_back(fallback_location: 'products#show')
 	end
+
+	def edit
+		@product = Product.find(params[:product_id])
+		@new_review = Review.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:product_id])
+		@new_review = Review.find(params[:id])
+
+		@new_review.comment = params[:review][:comment]
+
+		@new_review.save
+		redirect_back(fallback_location: 'products#show')
+	end
+
+	def destroy
+		@product = Product.find(params[:product_id])
+		@new_review = Review.find(params[:id])
+
+		@new_review.destroy
+		redirect_to product_path(@product)
+	end
 end
