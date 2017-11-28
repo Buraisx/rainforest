cc55dfe5b2dab98346343c04a91766c1261ff38f
Situ William <situ_william@yahoo.com>
2017-11-28 11:17:27 -0500

    completed new page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,20 @@ class ProductsController < ApplicationController
 	def show
 		@product = Product.find(params[:id])
 	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new
+
+		@product.name = params[:product][:name]
+		@product.description = params[:product][:description]
+		@product.price = params[:product][:price]
+		@product.url = params[:product][:url]
+
+		@product.save
+		redirect_to(products_path)
+	end
 end
