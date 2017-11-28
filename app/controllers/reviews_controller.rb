3c3c61bd79842558e5cffa57ce4730d10b150bc9
Situ William <situ_william@yahoo.com>
2017-11-28 15:46:38 -0500

    added review submit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,9 @@
 class ReviewsController < ApplicationController
-
+	def create
+		@review = Review.new
+		@review.comment = params[:review][:comment]
+		@review.product_id = params[:product_id]	
+		@review.save
+		redirect_back(fallback_location: 'products#show')
+	end
 end
